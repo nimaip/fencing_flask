64abbb31476abbd856561259ef2d2ca40f359f09
Nimai Ponna <nimaipponna@gmail.com>
2025-06-29 15:44:43 -0400

    Added SK

--- fencing_project/settings.py
+++ fencing_project/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-here')
+SECRET_KEY = os.environ.get('SECRET_KEY', '^*i-i9r6nhm95ndkpq=3_%lh-3=kcw18b^&!*7_rqtg!#w0!6l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
